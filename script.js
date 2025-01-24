062d0b6e4297955990931dae7d581f3de95b7c0d
Aman2205saini <137211234+Aman2205saini@users.noreply.github.com>
2025-01-24 17:56:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+ 
+	var name=document.getElementById("name").value
+	var age=document.getElementById("age").value
+
+	
+
+	const ageValidation=()=>{
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				if(parseInt(age)>=18){
+				resolve(`Welcome, ${name} You can vote`)
+			}else{
+				reject(`Oh sorry ${name} You aren't old enough`)
+			}
+			},4000)
+			
+		}				   
+	}
+	ageValidation()
+	    .then(messege=>(alert(messege)))
+	    .catch(messege=>(alert(messege)))
+})
